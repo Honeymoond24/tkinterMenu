ae975d5cb047ee6f9d4b0281d9a66c2066012763
tema9907 <kanal9907@gmail.com>
2022-02-27 22:18:08 +0600

    poka4tovkladkinerabotayt

--- applicationClass.py
+++ applicationClass.py
@@ -8,22 +8,23 @@ class Application(Tk):
         self.root.title("Ресторан \"Шакал\"")
         # Параметры по умолчанию
         self.fra = Frame(self.root, width=960, height=720, bg="White")
-        #self.fra.pack()
+        self.fra.pack()
         self.menu = AppMenu(self.root, self.fra)
         self.menu.add_menu()
+        
 
-    def notebooks(self):
-        nb = ttk.Notebook(self.root)
-        nb.pack(fill='both', expand='yes')
-        f1 = self.fra = Frame(self.root, width=960, height=720, bg="White")
-        self.fra.pack()
-        f2 = Text(self.root)
-        f3 = Text(self.root)
-
-        nb.add(f1, text='page1')
-        nb.add(f2, text='page2')
-        nb.add(f3, text='page3')
+    #def notebooks(self):
+        #nb = ttk.Notebook(self.root)
+        #nb.pack(fill='both', expand='yes')
+        #f1 = self.fra = Frame(self.root, width=960, height=720, bg="White")
+        #self.fra.pack()
+        #f2 = Text(self.root)
+        #f3 = Text(self.root)
+        #nb.add(f1, text='page1')
+        #nb.add(f2, text='page2')
+        #nb.add(f3, text='page3')
 
     def start(self):
-        self.notebooks()
+        #self.notebooks()
+       
         self.root.mainloop()
