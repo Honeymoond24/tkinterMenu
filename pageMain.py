e25e7c692fb3d9d2e0fad4b938c3e42d2d3e1976
Daniyal Z <zdaniyalr@gmail.com>
2022-03-01 10:48:39 +0600

    test grid

--- pageMain.py
+++ pageMain.py
@@ -10,10 +10,10 @@ class PageMain(tk.Frame):
 
         button1 = tk.Button(self, text="Go to Page One",
                             command=lambda: controller.show_frame("PageOne"))
+        button1.pack()
         button2 = tk.Button(self, text="Go to Page Two",
                             command=lambda: controller.show_frame("PageTwo"))
+        button2.pack()
         button3 = tk.Button(self, text="About",
                             command=lambda: controller.show_frame("PageAbout"))
-        button1.pack()
-        button2.pack()
         button3.pack()
