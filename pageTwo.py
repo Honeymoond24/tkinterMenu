aa957ca6ecf6374b286f9a9dbdf30d372b7089bb
tema9907 <kanal9907@gmail.com>
2022-03-02 01:45:13 +0600

    days

--- pageTwo.py
+++ pageTwo.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import *
+from card import Card
 
 from cms.header import *
+from foodcard import FoodCard
 
 
 class PageTwo(tk.Frame):
@@ -10,28 +12,48 @@ class PageTwo(tk.Frame):
         self.parent = parent
         self.controller = controller
 
-        top_frame = Frame(self, bg='cyan', pady=3)
+        top_frame = Frame(self, pady=3)
         Header(top_frame, self.controller)
-        center = Frame(self, bg='gray2', pady=3)
-        btm_frame = Frame(self, bg='white', pady=3)
-        btm_frame2 = Frame(self, bg='lavender', pady=3)
+        center = Frame(self, bg='#FEF2E4', pady=3)
         # layout all of the main containers
-        self.parent.grid_rowconfigure(1, weight=1)
-        self.parent.grid_columnconfigure(0, weight=1)
+        #self.parent.grid_rowconfigure(0, weight=1)
+        #self.parent.grid_columnconfigure(1, weight=2)
 
-        top_frame.grid(row=0, sticky="ns")
+        top_frame.grid(row=0, sticky="EW")
+       
         center.grid(row=1, sticky="nsew")
-        btm_frame.grid(row=3, sticky="ns")
-        btm_frame2.grid(row=4, sticky="ns")
+        label = tk.Label(center,background='#FD974F', text="Блюда на сегодня", font=controller.title_font)
+        label1 = tk.Label(center,background='#FD974F', text="Блюда на Завтра", font=controller.title_font)
+        self.cards = Card(center, self.controller, "карточка1", 'card1')
+        self.cards1 = Card(center, self.controller, "карточка2", 'card2')
+        self.cards2 = Card(center, self.controller, "карточка3", 'card3')
+        label.grid(row=0,column=0,columnspan=3,sticky="NSEW")
+        self.cards.grid(row=1, column=1)
+        self.cards1.grid(row=2, column=1)
+        self.cards2.grid(row=3, column=1)
+        self.cards3 = Card(center, self.controller, "карточка1", 'card1')
+        self.cards4 = Card(center, self.controller, "карточка2", 'card2')
+        self.cards5 = Card(center, self.controller, "карточка3", 'card3')
+        self.cards3.grid(row=1, column=2)
+        self.cards4.grid(row=2, column=2)
+        self.cards5.grid(row=3, column=2)
 
-        # create the center widgets
-        center.grid_rowconfigure(0, weight=1)
-        center.grid_columnconfigure(1, weight=1)
+        self.cards6 = Card(center, self.controller, "карточка1", 'card1')
+        self.cards7 = Card(center, self.controller, "карточка2", 'card2')
+        self.cards8 = Card(center, self.controller, "карточка3", 'card3')
+        label1.grid(row=0,column=3,columnspan=4,sticky="NSWE")
+        self.cards6.grid(row=1, column=3)
+        self.cards7.grid(row=2, column=3)
+        self.cards8.grid(row=3, column=3)
+        self.cards9 = Card(center, self.controller, "карточка1", 'card1')
+        self.cards10 = Card(center, self.controller, "карточка2", 'card2')
+        self.cards11 = Card(center, self.controller, "карточка3", 'card3')
+        self.cards9.grid(row=1, column=4)
+        self.cards10.grid(row=2, column=4)
+        self.cards11.grid(row=3, column=4)
 
-        # ctr_left = Frame(center, bg='blue', width=100, height=190)
-        ctr_mid = Frame(center, bg='yellow', width=250, height=190, padx=3, pady=3)
-        ctr_right = Frame(center, bg='green', width=100, height=190, padx=3, pady=3)
-
-        # ctr_left.grid(row=0, column=0, sticky="ns")
-        ctr_mid.grid(row=0, column=1, sticky="nsew")
-        ctr_right.grid(row=0, column=2, sticky="ns")
+        self.grid_rowconfigure(1, weight=10)
+        self.grid_columnconfigure(0, weight=10)
+        
+       
+        
