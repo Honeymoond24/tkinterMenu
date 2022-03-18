34c5233474e7579522f37e06b3f2d5bba9cf2f88
Daniyal Z <zdaniyalr@gmail.com>
2022-03-18 08:52:54 +0600

    otchet

--- applicationClass.py
+++ applicationClass.py
@@ -24,19 +24,20 @@ class Application(tk.Tk):  # Класс.
         # self.geometry(str(16 * 80 * self.multiplier) + "x" +  # 1280x720
         #               str(9 * 80 * self.multiplier) + "+50+50")
         self.geometry("1280x576+50+50")
-        container = tk.Frame(self, bg='#FEF2E4')  # Страницы
-        # container.place(x=0, y=0)
-        container.pack(side="top", fill="both", expand=False)
-        # container.grid_rowconfigure(0, weight=1)
-        # container.grid_columnconfigure(0, weight=1)
+        self.container = tk.Frame(self, bg='#FEF2E4')  # Страницы
+        # self.container.place(x=0, y=0)
+        self.container.pack(side="top", fill="both", expand=False)
+        # self.container.grid(row=0, column=0, sticky='nsew')
+        # self.container.grid_rowconfigure(0, weight=1)
+        # self.container.grid_columnconfigure(0, weight=1)
         self.frames = {}
 
-        self.frames["PageMain"] = PageMain(parent=container, controller=self)
-        self.frames["Food"] = Food(parent=container, controller=self)
-        self.frames["DailyMenu"] = DailyMenu(parent=container, controller=self)
-        # self.frames["PageAbout"] = PageAbout(parent=container, controller=self)
-        self.frames["PageCard"] = PageCard(parent=container, controller=self, link='link')
-        self.frames["PageStats"] = PageStats(parent=container, controller=self)
+        self.frames["PageMain"] = PageMain(parent=self.container, controller=self)
+        self.frames["Food"] = Food(parent=self.container, controller=self)
+        self.frames["DailyMenu"] = DailyMenu(parent=self.container, controller=self)
+        # self.frames["PageAbout"] = PageAbout(parent=self.container, controller=self)
+        self.frames["PageCard"] = PageCard(parent=self.container, controller=self, link='link')
+        self.frames["PageStats"] = PageStats(parent=self.container, controller=self)
 
         self.frames["PageMain"].grid(row=0, column=0, sticky="nsew")
         self.frames["Food"].grid(row=0, column=0, sticky="nsew")
@@ -46,10 +47,10 @@ class Application(tk.Tk):  # Класс.
         self.frames["PageStats"].grid(row=0, column=0, sticky="nsew")
 
         self.show_frame("PageMain")
-        self.menu = AppMenu(self, container)
+        self.menu = AppMenu(self, self.container)
         self.menu.add_menu()
         # self.menu.call_page_about()
 
     def show_frame(self, page_name):
-        frame = self.frames[page_name]
-        frame.tkraise()
+        self.frame = self.frames[page_name]
+        self.frame.tkraise()
--- pageStats.py
+++ pageStats.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from card import *
 from header import *
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class PageStats(tk.Frame):
@@ -9,50 +13,59 @@ class PageStats(tk.Frame):
         self.parent = parent  # Родительский элемент
         self.controller = controller  # это кто?
         # Фреймы, верстка
-        top_frame = Frame(self)
-        Header(top_frame, self.controller)
-        center = Frame(self)
+        self.top_frame = Frame(self)
+        Header(self.top_frame, self.controller)
+        self.center = Frame(self)
 
         # layout all of the main containers
         self.parent.grid_rowconfigure(1, weight=1)
         self.parent.grid_columnconfigure(0, weight=1)
 
-        top_frame.grid(row=0, sticky="ew")
-        center.grid(row=1, sticky="nsew")
+        self.top_frame.grid(row=0, sticky="ew")
+        self.center.grid(row=1, sticky="nsew")
 
         # create the center widgets
-        center.grid_rowconfigure(0, weight=1)
-        center.grid_columnconfigure(1, weight=1)
+        self.center.grid_rowconfigure(0, weight=1)
+        self.center.grid_columnconfigure(1, weight=1)
 
-        self.ctr_left = Frame(center, bg='#00bff6')
-        self.ctr_right = Frame(center,
-                               # bg="gray",
+        self.ctr_left = Frame(self.center, bg='#00bff6')
+        self.ctr_right = Frame(self.center,
                                width=512, height=50,
                                padx=20, pady=20)
 
         self.ctr_left.grid(row=0, column=0, sticky="ns")
         self.side_bar()
         self.ctr_right.grid(row=0, column=1, sticky="ns")
-        # self.cards = Card(self.ctr_right, self.controller, "Корейский суп", 'card1')
-        # self.cards1 = Card(self.ctr_right, self.controller, "Корейский суп", 'card2')
-        # self.cards2 = Card(self.ctr_right, self.controller, "Корейский суп", 'card3')
-        # self.cards.grid(row=0, column=1)
-        # self.cards1.grid(row=0, column=2)
-        # self.cards2.grid(row=0, column=3)
 
         # Переменные для боковой панели
-        # self.min_w = 50  # Minimum width of the frame
-        # self.max_w = 200  # Maximum width of the frame
-        # self.cur_width = self.min_w  # Increasing width of the frame
-        # self.expanded = False  # Check if it is completely expanded
-        self.stats_frame = Frame(self.ctr_right, bg="gray",
-                                 width=720, height=360,
-                                 padx=20, pady=20)
-        self.stats_frame.grid(row=0, column=0)
-        self.date_select = tk.Entry(self.ctr_right, width=20)
-        self.date_select.grid(row=1, column=0, padx=10, pady=10, sticky='e', rowspan=1)
-        # self.stats = tk.Entry(self.ctr_right, width=20)
-        # self.stats.grid(row=1, column=0, padx=10, pady=10, sticky='e', rowspan=1)
+        # self.stats_frame1 = Frame(self.ctr_right, bg="gray",
+        #                           width=720, height=360,
+        #                           padx=20, pady=20).pack()
+        self.canvas = tk.Canvas(self.ctr_right)
+
+        # self.frameChartsLT = Frame(self.stats_frame1)
+        # self.frameChartsLT.pack()
+        self.stockListExp = ['AMZN', 'AAPL', 'JETS', 'CCL', 'NCLH']
+        self.stockSplitExp = [15, 25, 40, 10, 10]
+        self.fig = Figure(
+            # figsize=(5, 4), dpi=100
+        )  # create a figure object
+        self.ax = self.fig.add_subplot(111)  # add an Axes to the figure
+        self.ax.pie(self.stockSplitExp, radius=2, labels=self.stockListExp, autopct='%0.2f%%', shadow=True, )
+        self.chart1 = FigureCanvasTkAgg(self.fig, self.canvas)
+        self.chart1.get_tk_widget().pack()
+        self.canvas.pack()
+        # self.canvas = FigureCanvasTkAgg(self.fig, self.stats_frame1)
+        # self.canvas.get_tk_widget().grid(row=0, column=0)
+
+        # self.stats_frame2 = Frame(self.ctr_right, bg="gray",
+        #                           width=720, height=360, padx=20, pady=20).grid(row=0, column=0)
+        # self.stats_frame3 = Frame(self.ctr_right, bg="gray",
+        #                           width=720, height=360, padx=20, pady=20).grid(row=0, column=0)
+        # self.stats_frame4 = Frame(self.ctr_right, bg="gray",
+        #                           width=720, height=360, padx=20, pady=20).grid(row=0, column=0)
+        # self.date_select = tk.Entry(self.ctr_right, width=20)
+        # self.date_select.grid(row=1, column=0, padx=10, pady=10, sticky='e', rowspan=1)
         self.grid_rowconfigure(1, weight=10)
         self.grid_columnconfigure(0, weight=1)
         print(f"{__name__} loaded")
