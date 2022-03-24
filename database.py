ba4d96ca304928e35ccb36d4f69d41668b6852a7
tema9907 <kanal9907@gmail.com>
2022-03-24 22:44:57 +0600

    database

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 import pyodbc
 
-
 class Database:
     def __init__(self):
         self.conn = pyodbc.connect(
             r'Driver={Microsoft Access Driver (*.mdb, *.accdb)}; '
-            r'DBQ=C:\\Users\\daniy\\OneDrive\\Рабочий стол\\Учеба\\Программная инженерия\\moduls\\mod2\\'
-            r'tkinterMenu\\sh_dababase.mdb;'
+            r'DBQ=C:\\Users\\Artem\Desktop\\lab2\\sh_dababase.mdb;'
+          
         )
         self.cursor = self.conn.cursor()
 
@@ -15,12 +14,13 @@ class Database:
     #                         (dish_id, dish_name, description, picture, weight, calories, dish_type, cost))
     #     self.conn.commit()
 
-    def select(self):
-        self.cursor.execute("""SELECT * FROM dish""")
-        for row in self.cursor.fetchall():
-            print(row)
+    def select(self,a):
+        self.cursor.execute(a)
+        return self.cursor.fetchall()
+        #for row in self.cursor.fetchall():
+            #print(row)
 
 
-db = Database()
-# db.call('id2', 'name1')
-db.select()
+# db = Database()
+# # db.call('id2', 'name1')
+# db.select()
